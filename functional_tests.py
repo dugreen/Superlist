7f679395eabca089580c8cd3874d9e6185a087df
dugreen <13114956717@163.com>
2017-11-13 16:17:06 +0800

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -17,16 +17,31 @@ class NewVisitorTest(unittest.TestCase):
 
         # 我注意到网页的标题和头部都包含“To-Do”这个词
         assert 'To-Do' in self.browser.title,"Browser title was " + self.browser.title
-        self.fail('finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do',header_text)
 
         # 应用邀请我输入一个待办事项
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
-        # 我在文本框中输入了“buy peacock feathers”(购买孔雀羽毛)
+        # 我在文本框中输入了“Buy peacock feathers”(购买孔雀羽毛)
         # 我的爱好是使用假蝇做児钓鱼
+        inputbox.send_keys('Buy peacock feathers')
 
         # 我按回车后，页面更新了
         # 待办事项表格中显示了“1：Buy peacock feathers”
+        inputbox.send_keys(Keys.ENTER)
 
+        table = self.browser.find_element_by_id(id_list_table)
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1:Buy peacock feathers' for row in rows)
+        )
+
+        self.fail('Finish the test!')
         # 页面中又显示了一个文本框，可以输入其他的待办事项
         # 我输入“Use peacock feathers to make a fly”
         # 我做事很有条理
