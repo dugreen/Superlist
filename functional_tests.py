5d3fb583f664625221eec3f00fb0640cf68063dd
dugreen <13114956717@163.com>
2017-11-13 19:30:19 +0800

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import unittest
+from selenium.webdriver.common.keys import Keys
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
@@ -35,10 +36,11 @@ class NewVisitorTest(unittest.TestCase):
         # 待办事项表格中显示了“1：Buy peacock feathers”
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id(id_list_table)
-        rows = table.find_element_by_tag_name('tr')
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1:Buy peacock feathers' for row in rows)
+            any(row.text == '1:Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table!"
         )
 
         self.fail('Finish the test!')
